7ab3af5c3919709a0d216899129441db34d7211f
w hy <ggliker@qq.com>
2013-08-27 21:03:45 +0800

    add lose UIStatusBar fix code demo

---
+++ NavAppLoseBar/NavAppLoseBar/GAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAppDelegate.h
+//  NavAppLoseBar
+//
+//  Created by Onyx on 27/8/13.
+//  Copyright (c) 2013 Onyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NavAppLoseBar/NavAppLoseBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavAppLoseBar
+//
+//  Created by Onyx on 27/8/13.
+//  Copyright (c) 2013 Onyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAppDelegate class]));
+    }
+}
